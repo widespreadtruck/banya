6c26dac8c51f53d8185670292b8f6f0ffd1b2814
Nikita Guliaev <gulnikita63@gmail.com>
2020-12-07 23:54:52 -0500

    added gallery

--- assets/js/main.js
+++ assets/js/main.js
@@ -283,3 +283,86 @@ window.addEventListener("resize", heroSlider().resize);
 // ***************************
 // Modal functions END
 // ***************************
+
+
+// ***************************
+// LIGHTBOX start
+// ***************************
+// Open the Modal
+
+function openModal() {
+    document.getElementById("myModal").style.display = "block";
+}
+
+// Close the Modal
+function closeModal() {
+    document.getElementById("myModal").style.display = "none";
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("demo");
+
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" activeThumbnail", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " activeThumbnail";
+
+    // if (n > slides.length) { slideIndex = 1 }
+    // if (n < 1) { slideIndex = slides.length }
+    // for (i = 0; i < slides.length; i++) {
+    //     slides[i].style.display = "none";
+    // }
+    // for (i = 0; i < dots.length; i++) {
+    //     dots[i].className = dots[i].className.replace(" activeThumbnail", "");
+    // }
+    // slides[slideIndex - 1].style.display = "block";
+    // dots[slideIndex - 1].className += " activeThumbnail";
+
+}
+
+document.addEventListener('keydown', function(e) {
+    switch(e.key) {
+        case "ArrowLeft":
+            plusSlides(-1);
+            break;
+        case "ArrowUp":
+            plusSlides(-1);
+            break;
+        case "ArrowDown":
+            plusSlides(1);
+            break;
+        case "ArrowRight":
+            plusSlides(1);
+            break;
+        case "Escape":
+            closeModal();
+        }
+});
+
+
+
+
+// ***************************
+// LIGHTBOX end
+// **************************
