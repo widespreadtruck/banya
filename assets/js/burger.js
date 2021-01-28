a2103804d489687dd8adb4a714fbb694ce2d7860
Nikita Guliaev <gulnikita63@gmail.com>
2021-01-28 14:22:27 -0500

    burger works on safari

--- assets/js/burger.js
+++ assets/js/burger.js
@@ -1,78 +1,26 @@
-// ***************************
-//  burger menu start
-// **************************
-const burgerIcon = document.getElementById('burgerIcon');
-const burgerMenu = document.getElementById('burgerMenu');
-const checkbox = document.getElementById("checkbox");
+$(function() {
+    function burgerMenu() {
+        let icon = $("#burgerIcon");
+        let burgerMenu = $('#burgerMenu');
+        let links = $("#burgerMenu").find(".burgerMenu__link");
+        icon.on("click", (e)=>{
+            e.preventDefault();
+            toggleMenu();
+        });
 
-// const openCloseBurger = () => {
-//     burgerIcon.classList.toggle("change");
-//     if (checkbox.checked) {
-//         burgerMenu.style.right = "0%";
-//     } else {
-//         burgerMenu.style.right = "-100%";
-//     }
-// }
-burgerIcon.addEventListener("click", () => {
-    burgerIcon.classList.toggle("change");
-    if (checkbox.checked) {
-        burgerMenu.style.right = "0%";
-    } else {
-        burgerMenu.style.right = "-100%";
-    }
-}, false);
+        links.on("click", () => toggleMenu());
 
-burgerIcon.addEventListener("touchstart", () => {
-    burgerIcon.classList.toggle("change");
-    if (checkbox.checked) {
-        burgerMenu.style.right = "0%";
-    } else {
-        burgerMenu.style.right = "-100%";
-    }
-}, false);
+        function toggleMenu() {
+            icon.toggleClass("change");
+            if (icon.hasClass("change")) {
+                burgerMenu.css("right", "0%");
+            } else {
+                burgerMenu.css("right", "-100%");
+            }
+        };
+    };
 
-
-const menuItems = document.querySelectorAll("#burgerMenu a");
-
-menuItems.forEach(function (navElement) {
-    navElement.onclick = function () {
-        checkbox.checked = false;
-        openCloseBurger();
-    }
+    burgerMenu();
 });
 
-window.onclick = function(event) {
-    if (event.target == burgerMenu) {
-        checkbox.checked = false;
-        openCloseBurger();
-    }
-};
 
-// if (document.getElementById("myModal").style.display = "block") {
-//     // checkbox.checked = false;
-//     // openCloseBurger();
-//     burgerIcon.style.display = 'none';
-// }
-
-
-// ***************************
-//  burger menu end
-// **************************
-
-// document.addEventListener('click', function(event) {
-//     if (!event.target.closest(".burgerMenu")) return;
-    
-//     checkbox.checked = false;
-//     openCloseBurger();
-    
-// }, false)
-
-
-// const openCloseBurger = () => {
-//     document.getElementById('burgerIcon').classList.add("change");
-// }
-
-// const burger = document.getElementById('burgerIcon');
-// burger.onclick = function() {
-//     document.getElementById('burgerIcon').classList.toggle('change')
-// }
